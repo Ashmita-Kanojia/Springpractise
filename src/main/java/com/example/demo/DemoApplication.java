0d8c586ce4bb06f26281a343c9699b62ac4e809c
Ashmita Kanojia -X (akanojia - ZENSAR TECHNOLOGIES LIMITED at Cisco) <akanojia@partnet.cisco.com>
2020-04-22 13:16:23 +0530

    I18N first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,18 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+		resolver.setDefaultLocale(Locale.US);
+		return resolver;
+
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messages() {
+		ResourceBundleMessageSource message = new ResourceBundleMessageSource();
+		message.setBasename("message");
+		return message;
+	}
 }
--- src/main/java/com/example/demo/hello/HelloWorld.java
+++ src/main/java/com/example/demo/hello/HelloWorld.java
@@ -1,24 +1,38 @@
 package com.example.demo.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorld {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 
 	// Simple Method
 	// URI : /helloWorld
 	// HTTP Method - GET
 	// Two annotation to do @RequestMapping & @GetMapping
-	//@RequestMapping(method = RequestMethod.GET, path="/helloWorld")
+	// @RequestMapping(method = RequestMethod.GET, path="/helloWorld")
 	@GetMapping("/helloworld2")
 	public String helloWorld() {
 		return "HelloWorld";
 	}
-	
+
 	@GetMapping("/getUserDetails")
 	public UserDetails getUserDetails() {
 		return new UserDetails("Ashmita", "Kanojia", "Nashik");
+	}
+
+	@GetMapping("/I18NDemo")
+	public String getMessagesInI18NFormat(@RequestHeader("Locale") String locale) {
+
+		return messageSource.getMessage("label.hello",null, new Locale(locale));
 	}
 }
