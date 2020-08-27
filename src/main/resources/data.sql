6c9b272442f4a52729c02e611a11a0143470ba3b
user <asshukanojia@gmail.com>
2020-08-27 14:28:32 +0530

    added address in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user_Table values(101, 'kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
-insert into user_Table values(102, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
-insert into user_Table values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark'); 
+insert into user_Table values(101,'B-10','kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin', 'ssn101', 'kreddy');
+insert into user_Table values(102,'A-10','gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into user_Table values(103,'C-10', 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark'); 
